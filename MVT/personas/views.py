675a30b15008aae34978fd2275d989532942e3ac
Dario Sbarbati <dario.sbarbati@gmail.com>
2022-07-25 15:52:23 -0300

    Listo

--- MVT/personas/views.py
+++ MVT/personas/views.py
@@ -3,10 +3,11 @@ import re
 from django.shortcuts import render
 from personas.models import Persona
 
-#Creo la funcion para agregar personas a la odb
+# Creo la funcion para agregar personas a la odb
+# Antes de ir a http://127.0.0.1:8000/crear_persona/ modificar los siguientes parametros y reiniciar servidor
 
 def create_person(resquest):
-    new_person=Persona.objects.create(name="Leandro", last_name="Sbarbati", parent="Hermano", ages=42)
+    new_person=Persona.objects.create(name="--", last_name="--", parent="--", ages=0)
     context={
         "new_person":new_person,
     }
