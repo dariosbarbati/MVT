1f4c63bd4b79357d2d778331dabc692d00d43ab6
Dario Sbarbati <dario.sbarbati@gmail.com>
2022-07-25 22:21:12 -0300

    control de error

--- MVT/personas/views.py
+++ MVT/personas/views.py
@@ -7,7 +7,7 @@ from personas.models import Persona
 # Antes de ir a http://127.0.0.1:8000/crear_persona/ modificar los siguientes parametros y reiniciar servidor
 
 def create_person(resquest):
-    new_person=Persona.objects.create(name="--", last_name="--", parent="--", ages=0)
+    new_person=Persona.objects.create(name="", last_name="", parent="", ages=0)
     context={
         "new_person":new_person,
     }
